e2546b5a015c5bcf545bb7ea8cfd211dc0827e25
Frank Bernal <frankbernal@mail.fresnostate.edu>
2025-10-19 18:58:08 -0700

    finished getOutputCalculations(). Works 4.0

--- CWI RBC/component_prototypes/components.jsx
+++ CWI RBC/component_prototypes/components.jsx
@@ -366,6 +366,7 @@ const getOutputCalculations = (formData) => {
   const netBenefitPerAF = formData.value_stored_water - totalAnnualCostPerAF;
   const ftPerDayToCfs = 1.98
   const rechargeFlowCfs = avgAnnualRechargeDepth / ftPerDayToCfs;
+  const fillRate54InPipe = rechargeFlowCfs * 1.5; // Placeholder value for fill rate in cfs
 
 
   const outputs = [
@@ -506,6 +507,14 @@ const getOutputCalculations = (formData) => {
       unit_cost_units: "",
       cost: null,
       cost_per_acre: null
+     },
+     { label: "Fill Rate (54\" pipe)",
+      quantity: fillRate54InPipe,
+      quantity_unit: "cfs",
+      unit_cost: null,
+      unit_cost_units: "",
+      cost: null,
+      cost_per_acre: null
      }
   ];
 
@@ -513,8 +522,6 @@ const getOutputCalculations = (formData) => {
 }
 
 function OutputsDataSection({ formData }) {
-  const outs = getOutputCalculations(formData);
-  
   // helper functions
   const toPrice = (num) => {
     if (num === null) return "-";
@@ -536,177 +543,9 @@ function OutputsDataSection({ formData }) {
     return ` / ${unit}`;
   }
 
-  // used for annual capital payment
-  const pmt = (rate, nper, pv) => {
-    if (rate === 0) return -(pv / nper);
-    return (rate * pv) / (1 - Math.pow(1 + rate, -nper));
-  }
-
-  // Calculations and values for Outputs Section
   const engineeringFirms = getEngineeringFirms();
-  const calculations = getCalculationsData(formData);
-  const landCost = formData.ac_pond * formData.land_cost_per_acre;
-  const pipelineInletCost = 20000;
-  const pipelineCostPerFt = 200;
-  const pipelineTotalCost = formData.pipeline_length * pipelineCostPerFt;
-  const fencingQty = 0;
-  const fencingCostPerFt = 6; // Placeholder value for fencing cost per ft
-  const fencingTotalCost = fencingQty * fencingCostPerFt;
-  const subtotal = landCost + calculations.total_cost_of_earthwork + pipelineInletCost + pipelineTotalCost + fencingTotalCost;
-  const engineeringPercentage = 0.2;
-  const engineeringCost = subtotal * engineeringPercentage;
-  const totalCostEstimate = subtotal + engineeringCost;
-  const annualCapitalPayment = pmt(formData.annual_interest_rate / 100, formData.loan_length, totalCostEstimate);
-  const avgAnnualRechargeDepth = formData.infiltration_rate * calculations.wetted_area_acres;
-  const [annualEvapLossNotIncluded, setAnnualEvapLossNotIncluded] = useState(30); // Use for user manip later
-  const netRecharge = (avgAnnualRechargeDepth * 30 * formData.num_wet_months * (formData.wet_year_freq / 100) * (1 - (annualEvapLossNotIncluded / 100)));
-  const annualCapitalCostPerAF = annualCapitalPayment / netRecharge;
-  const totalAnnualCostPerAF = annualCapitalCostPerAF + formData.cost_recharge_water + formData.cost_om;
-  const netBenefitPerAF = formData.value_stored_water - totalAnnualCostPerAF;
-  const ftPerDayToCfs = 1.98
-  const rechargeFlowCfs = avgAnnualRechargeDepth / ftPerDayToCfs;
+  const outputs = getOutputCalculations(formData);
 
-
-  const outputs = [
-    { label: "Land Purchase", 
-      quantity: formData.ac_pond, 
-      quantity_unit: "acres", 
-      unit_cost: formData.land_cost_per_acre, 
-      unit_cost_units: "acre",
-      cost: landCost, 
-      cost_per_acre: formData.land_cost_per_acre 
-    },
-    { label: "Earthwork",
-      quantity: calculations.total_volume_of_earthwork,
-      quantity_unit: "cubic yards",
-      unit_cost: formData.earthwork_cost_per_cy,
-      unit_cost_units: "cubic yard",
-      cost: calculations.total_cost_of_earthwork,
-      cost_per_acre: (calculations.total_cost_of_earthwork / formData.ac_pond)
-    },
-    { label: "Pipeline Inlets",
-      quantity: 1,
-      quantity_unit: "each",
-      unit_cost: pipelineInletCost,
-      unit_cost_units: "each",
-      cost: pipelineInletCost,
-      cost_per_acre: (pipelineInletCost / formData.ac_pond)
-    },
-    { label: "Pipeline (30)",
-      quantity: formData.pipeline_length,
-      quantity_unit:"feet",
-      unit_cost: pipelineCostPerFt,
-      unit_cost_units: "foot",
-      cost: pipelineTotalCost,
-      cost_per_acre: (pipelineTotalCost / formData.ac_pond)
-    },
-    // Add input variable for fencing 
-    { label: "Fencing",
-      quantity: 0,
-      quantity_unit: "feet",
-      unit_cost: fencingCostPerFt,
-      unit_cost_units: "foot",
-      cost: fencingTotalCost,
-      cost_per_acre: (fencingTotalCost / formData.ac_pond)
-    },
-    // keeping same shape for object to not break table structure
-    { label: "Subtotal",
-      quantity: null,
-      quantity_unit: "",
-      unit_cost: null,
-      unit_cost_units: "",
-      cost: subtotal,
-      cost_per_acre: subtotal / formData.ac_pond
-    },
-    { label: `Engineering and Contingency (${engineeringPercentage * 100}%)`,
-      quantity: null,
-      quantity_unit: "",
-      unit_cost: null,   
-      unit_cost_units: "",
-      cost: engineeringCost,
-      cost_per_acre: engineeringCost / formData.ac_pond
-    },
-     { label: "Total Cost Estimate",
-      quantity: null,
-      quantity_unit: "",
-      unit_cost: null,
-      unit_cost_units: "",
-      cost: totalCostEstimate,
-      cost_per_acre: totalCostEstimate / formData.ac_pond
-     },
-     { label: "Annual Capital Payment",
-      quantity: null,
-      quantity_unit: "",
-      unit_cost: annualCapitalPayment,
-      unit_cost_units: "year",
-      cost: null,
-      cost_per_acre: null
-     },
-     { label: "Average Annual Recharge Depth",
-      quantity: avgAnnualRechargeDepth,
-      quantity_unit: "feet / day",
-      unit_cost: null,
-      unit_cost_units: "",
-      cost: null,
-      cost_per_acre: null
-     },
-     { label: "Net Recharge (Applied Water - Evaporation Loss)",
-      quantity: netRecharge,
-      quantity_unit: "acre-feet / year",
-      unit_cost: null,
-      unit_cost_units: "",
-      cost: null,
-      cost_per_acre: null
-     },
-     { label: "Annual Capital Cost per Acre-Foot",
-      quantity: null,
-      quantity_unit: "",
-      unit_cost: annualCapitalCostPerAF,
-      unit_cost_units: "acre-foot",
-      cost: null,
-      cost_per_acre: null
-     },
-     { label: "Water Purchase Cost of Recharge Water",
-      quantity: null,
-      quantity_unit: "",
-      unit_cost: formData.cost_recharge_water,
-      unit_cost_units: "acre-foot",
-      cost: null,
-      cost_per_acre: null
-     },
-     { label: "O&M Cost for Recharge and Basin Maintenance",
-      quantity: null,
-      quantity_unit: "",
-      unit_cost: formData.cost_om,
-      unit_cost_units: "acre-foot",
-      cost: null,
-      cost_per_acre: null
-     },
-     { label: "Total Annual Cost per Acre-Foot of Recharged Water",
-      quantity: null,
-      quantity_unit: "",
-      unit_cost: totalAnnualCostPerAF,
-      unit_cost_units: "acre-foot",
-      cost: null,
-      cost_per_acre: null
-     },
-     { label: "Net Benefit per Acre-Foot",
-      quantity: null,
-      quantity_unit: "",
-      unit_cost: netBenefitPerAF,
-      unit_cost_units: "acre-foot",
-      cost: null,
-      cost_per_acre: null
-     },
-     { label: "Recharge Flow",
-      quantity: rechargeFlowCfs,
-      quantity_unit: "cfs",
-      unit_cost: null,
-      unit_cost_units: "",
-      cost: null,
-      cost_per_acre: null
-     }
-  ]
   return (
     <div className="outputs-data-section">
       <fieldset className="outputs-fieldset">
@@ -741,6 +580,8 @@ function OutputsDataSection({ formData }) {
               <li key={firm.name}><a href={firm.url} target="_blank">{firm.name}</a></li>
             ))}
           </ul>
+          <p>Annual loss of crop profits not included - If you assume almonds as the crop, the annual loss of profit is $0</p>
+          <p>Annual evaporation losses not included - assume that equals 30%</p>
         </div>
       </fieldset>
     </div>
