2cacbdcb4d99d56635d8356330bf2f3b7742d202
Frank Bernal <frankbernal@mail.fresnostate.edu>
2025-10-30 08:27:07 -0700

    ConceptDesign styled and stand-alone

--- CWI RBC/src/components/ConceptDesign.jsx
+++ CWI RBC/src/components/ConceptDesign.jsx
@@ -1,7 +1,20 @@
 export default function ConceptDesign() {
+  const styles = {
+    container: {
+      // Moved from App.css (#concept-design-section + .has-shadow)
+      backgroundColor: '#fff',
+      color: '#000',
+      padding: '3rem',
+      textAlign: 'center',
+      margin: 0,
+      height: '100vh', // Change later if needed
+      boxShadow: 'black 0px 0px 10px -1px',
+    },
+  };
+
   return (
-    <div className="has-shadow" id="concept-design-section">
+    <div id="concept-design-section" style={styles.container}>
       <h2>Concept Design Placeholder</h2>
     </div>
-  )
+  );
 }
